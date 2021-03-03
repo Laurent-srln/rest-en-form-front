53a0754812c96ba8e06723ccbebe7eb7e14c30c8
julienblotiere <julienblotiere@live.fr>
2021-03-03 16:05:12 +0100

    component footer structure done

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,7 +7,9 @@ import './styles.scss';
 // == Composant
 const Footer = () => (
   <footer className="footer">
-    <p>Footer</p>
+    <a className="footer__team" href="#">L'équipe</a>
+    <a className="footer__coachs" href="">Les coachs</a>
+    <a className="footer__legalNotice" href="">Mentions légales</a>
   </footer>
 );
 
