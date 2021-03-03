4827440755591cfac2382b66fb129ce1900f1c59
Gwenael Cotton <gcotton.oclock@gmail.com>
2021-03-03 23:57:19 +0100

    update component dashboardCoach and styles

--- src/components/DashboardCoach/index.js
+++ src/components/DashboardCoach/index.js
@@ -7,69 +7,73 @@ const DashboardCoach = () => (
     <div className="dashboard-container">
       <div className="dashboard-next-coaching">
         <h2 className="dashboard-next-coaching-title">Prochain coachings</h2>
-        <ul className="dashboard-list-info">
-          <li>Date</li>
-          <li>Heure</li>
-          <li>Adhérent</li>
-        </ul>
-        <ul className="dashboard-list-info-item">
-          <li>Mardi 6 févirer</li>
-          <li>9h15-9h30</li>
-          <li>Jean</li>
-        </ul>
-        <ul className="dashboard-list-info-item">
-          <li>Mardi 6 févirer</li>
-          <li>9h15-9h30</li>
-          <li>Jean</li>
-        </ul>
-        <ul className="dashboard-list-info-item">
-          <li>Mardi 6 févirer</li>
-          <li>9h15-9h30</li>
-          <li>Jean</li>
-        </ul>
-        <ul className="dashboard-list-info-item">
-          <li>Mardi 6 févirer</li>
-          <li>9h15-9h30</li>
-          <li>Jean</li>
-        </ul>
-        <ul className="dashboard-list-info-item">
-          <li>Mardi 6 févirer</li>
-          <li>9h15-9h30</li>
-          <li>Jean</li>
-        </ul>
+        <div className="dashboard-next-coaching-container">
+          <ul className="dashboard-list-info">
+            <li>Date</li>
+            <li>Heure</li>
+            <li>Adhérent</li>
+          </ul>
+          <ul className="dashboard-list-info-item">
+            <li>Mardi 6 févirer</li>
+            <li>9h15-9h30</li>
+            <li>Jean</li>
+          </ul>
+          <ul className="dashboard-list-info-item">
+            <li>Mardi 6 févirer</li>
+            <li>9h15-9h30</li>
+            <li>Jean</li>
+          </ul>
+          <ul className="dashboard-list-info-item">
+            <li>Mardi 6 févirer</li>
+            <li>9h15-9h30</li>
+            <li>Jean</li>
+          </ul>
+          <ul className="dashboard-list-info-item">
+            <li>Mardi 6 févirer</li>
+            <li>9h15-9h30</li>
+            <li>Jean</li>
+          </ul>
+          <ul className="dashboard-list-info-item">
+            <li>Mardi 6 févirer</li>
+            <li>9h15-9h30</li>
+            <li>Jean</li>
+          </ul>
+        </div>
       </div>
       <div className="dashboard-last-coaching">
         <h2 className="dashboard-last-coaching-title">Dernier coachings</h2>
-        <ul className="dashboard-list-info">
-          <li>Date</li>
-          <li>Heure</li>
-          <li>Adhérent</li>
-        </ul>
-        <ul className="dashboard-list-info-item">
-          <li>Mardi 6 févirer</li>
-          <li>9h15-9h30</li>
-          <li>Jean</li>
-        </ul>
-        <ul className="dashboard-list-info-item">
-          <li>Mardi 6 févirer</li>
-          <li>9h15-9h30</li>
-          <li>Jean</li>
-        </ul>
-        <ul className="dashboard-list-info-item">
-          <li>Mardi 6 févirer</li>
-          <li>9h15-9h30</li>
-          <li>Jean</li>
-        </ul>
-        <ul className="dashboard-list-info-item">
-          <li>Mardi 6 févirer</li>
-          <li>9h15-9h30</li>
-          <li>Jean</li>
-        </ul>
-        <ul className="dashboard-list-info-item">
-          <li>Mardi 6 févirer</li>
-          <li>9h15-9h30</li>
-          <li>Jean</li>
-        </ul>
+        <div className="dashboard-last-coaching-container">
+          <ul className="dashboard-list-info">
+            <li>Date</li>
+            <li>Heure</li>
+            <li>Adhérent</li>
+          </ul>
+          <ul className="dashboard-list-info-item">
+            <li>Mardi 6 févirer</li>
+            <li>9h15-9h30</li>
+            <li>Jean</li>
+          </ul>
+          <ul className="dashboard-list-info-item">
+            <li>Mardi 6 févirer</li>
+            <li>9h15-9h30</li>
+            <li>Jean</li>
+          </ul>
+          <ul className="dashboard-list-info-item">
+            <li>Mardi 6 févirer</li>
+            <li>9h15-9h30</li>
+            <li>Jean</li>
+          </ul>
+          <ul className="dashboard-list-info-item">
+            <li>Mardi 6 févirer</li>
+            <li>9h15-9h30</li>
+            <li>Jean</li>
+          </ul>
+          <ul className="dashboard-list-info-item">
+            <li>Mardi 6 févirer</li>
+            <li>9h15-9h30</li>
+            <li>Jean</li>
+          </ul>
+        </div>
       </div>
     </div>
     <a href="https://github.com/" className="dashboard-link">Voir les adhérents </a>
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
+import logo from './react-logo.svg';
 import './styles.scss';
 
 const Header = () => (
   <header className="header">
-    <img className="header-logo" src="#" alt="Logo O'Sport" />
+    <img className="header-logo" src={logo} alt="Logo O'Sport" />
   </header>
 );
 
