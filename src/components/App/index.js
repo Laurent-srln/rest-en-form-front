9b01a620c9944f093f6d4c4f755e8a698857fca5
Mathilde-Frere <mathilde.frere1@gmail.com>
2021-03-04 17:44:18 +0100

    Coachings and Coaching components : basic structure and style

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -4,9 +4,10 @@ import React from 'react';
 // == Import
 import Header from 'src/components/Header';
 import Footer from 'src/components/Footer';
-import Login from 'src/components/Login';
+// import Login from 'src/components/Login';
 // import DashboardCoach from 'src/components/DashboardCoach';
 // import DashboardAdh from 'src/components/DashboardAdh';
+import Coachs from 'src/components/Coachs';
 
 import './styles.scss';
 
@@ -14,9 +15,10 @@ import './styles.scss';
 const App = () => (
   <div className="app">
     <Header />
-    <Login />
+    {/* <Login /> */}
     {/* <DashboardCoach /> */}
     {/* <DashboardAdh /> */}
+    <Coachs />
     <Footer />
   </div>
 );
